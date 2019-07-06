01f37f4987e39e8e508b70a34f66e2d5ae43305d
Anton Tsivarev <tsivarev.a@gmail.com>
2019-07-06 13:16:20 +0300

    Updates

---
+++ precache-manifest.3cd41d75ab4dcfad867682e7115a5cff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "76b6ba30c1e24daa3629",
+    "url": "/mini-app-file-api/static/js/runtime~main.ba73f3b2.js"
+  },
+  {
+    "revision": "947335d4e51c6697a415",
+    "url": "/mini-app-file-api/static/js/main.e133d269.chunk.js"
+  },
+  {
+    "revision": "28a73000753dea54a6be",
+    "url": "/mini-app-file-api/static/js/2.3ae36eba.chunk.js"
+  },
+  {
+    "revision": "28a73000753dea54a6be",
+    "url": "/mini-app-file-api/static/css/2.63dc03ba.chunk.css"
+  },
+  {
+    "revision": "11f2cf6c148295787c121643c7c13d95",
+    "url": "/mini-app-file-api/index.html"
+  }
+];
